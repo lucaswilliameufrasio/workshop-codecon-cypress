103b899936b9d7e4ea1a140c3aceae907d15f374
Lucas William <lucaswilliameufrasio@gmail.com>
2020-09-10 17:01:55 -0300

    chore: add dashboard locators support file

---
+++ cypress/support/dash_locator.js
@@ -0,0 +1,7 @@
+const dash_locator = {
+  dash_perfil_btn: ".top-user-name > .btn",
+  admin_option_modal: ".item-admin.item-admin",
+  users_option_sidebar: ".menu-main > :nth-child(4) > .menu-target",
+};
+
+export default dash_locator;
